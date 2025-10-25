0b529db64eba83b83397f39cab35c3446fc6bd18
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2025-10-25 10:41:51 +0200

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,7 @@
+ * Be able to install other apps (long term)
+ * Create projects, apps
+ * Compile the project
+ * Run the project
+ * Debug the project
+ * Manage dependencies
+ * Create interface layer
---
+++ src/applications/lumos/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <utility>
+#include <cstdlib>
+#include <sstream>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+int main()
+{
+    std::cout << "LumosTool Simple Application" << std::endl;
+    return 0;
+}
